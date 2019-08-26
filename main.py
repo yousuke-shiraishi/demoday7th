fbc145f1234f9254c59ea22a65ea06c188f3619f
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2019-08-26 22:57:15 +0900

    first_commit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # -*- coding: UTF-8 -*-
 import shutil
-# request フォームから送信した情報を扱うためのモジュール
-# redirect  ページの移動
-# url_for アドレス遷移
+import tempfile
+import requests
 from flask import redirect, url_for, render_template, flash, Flask, request, abort
 # ファイル名をチェックする関数
 from werkzeug.utils import secure_filename
@@ -17,19 +16,6 @@ import glob
 from PIL import Image
 import pickle
     
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
 
 import os
 import sys
@@ -48,8 +34,12 @@ from argparse import ArgumentParser
 
 from os.path import join, dirname
 from dotenv import load_dotenv
+import psycopg2
+import boto3
+from boto3.session import Session
 
 load_dotenv(verbose=True)
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -69,18 +59,13 @@ app = Flask(__name__)
 # line_bot_api = LineBotApi(channel_access_token)
 # handler = WebhookHandler(channel_secret)
 
+# DATABASE_URL = os.environ['DATABASE_URL']
+
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+
 SUB_DIR = 'actress/'
-
-
-app = Flask(__name__)
-
-cloudinary.config.update = ({
-    'cloud_name':os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'api_key': os.environ.get('CLOUDINARY_API_KEY'),
-    'api_secret': os.environ.get('CLOUDINARY_API_SECRET')
-})
-
-
 app.secret_key = os.getenv('SECRET_KEY', 'for dev')
 SAVE_DIR = "./images"
 if not os.path.isdir(SAVE_DIR):
@@ -91,10 +76,58 @@ IMG_DIR = './images_stock/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app.config['UPLOAD_FOLDER'] = SAVE_DIR
 
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_REGION_NAME = os.environ['AWS_REGION_NAME']
+
+
+POSTG_ID = os.environ['PG_ID']
+POSTG_PW = os.environ['PG_PW']
+POSTG_DB = os.environ['PG_DB']
+
 estimated_d =[]
 img1 =[]
 exists_img=[]
 img_url=""
+
+# s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
+def get_public_url(bucket, target_object_path):
+    """
+    対象のS3ファイルのURLを取得する
+
+    Parameters
+    ----------
+    bucket: string
+        S3のバケット名
+    target_object_path: string
+        取得したいS3内のファイルパス
+
+    Returns
+    ----------
+    url: string
+        S3上のオブジェクトのURL
+    """
+    bucket_location = s3.get_bucket_location(Bucket=bucket)
+    return "https://s3-{0}.amazonaws.com/{1}/{2}".format(
+        bucket_location['LocationConstraint'],
+        bucket,
+        target_object_path)
+
+
+
+# def imread_web(url):
+#     # 画像をリクエストする
+#     res = requests.get(url)
+#     img = None
+#     # Tempfileを作成して即読み込む
+#     with tempfile.NamedTemporaryFile(dir='./') as fp:
+#         fp.write(res.content)
+#         fp.file.seek(0)
+#         img = cv2.imread(fp.name,cv2.IMREAD_GRAYSCALE)
+#     return img
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -140,8 +173,6 @@ def upload():
     img = cv2.imdecode(img_array, 1)
     img_size = (200, 200)
     ret = {}
-    com_img_dists = {}
-    
     Img =  Image.open(img1)
     dt_now = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_%f")
     save_path = os.path.join(SAVE_DIR, dt_now + ".jpeg")
@@ -149,17 +180,6 @@ def upload():
 
     img1 = glob.glob(save_path)
     img_url = img1[0]
-
-    img_dir_files = cloudinary.api.resources(
-    type = "upload", 
-    prefix = "actress")
-    f1 = open('file_name_texts','wb')
-    pickle.dump(img_dir_files,f1)
-    f1.close
-    
-    # f = open('file_name_texts','rb')
-    # img_dir_files = pickle.load(f)
-
     
     #####################################3
 
@@ -173,28 +193,30 @@ def upload():
         # detector = cv2.ORB_create()
         detector = cv2.AKAZE_create()
         (_, target_des) = detector.detectAndCompute(target_img, None)
+        conn = psycopg2.connect(
+        host = "0.0.0.0",
+        port = 5432,
+        database=POSTG_DB,
+        user=POSTG_ID,
+        password=POSTG_PW)
+        c = conn.cursor()
+        c.execute('SELECT * FROM flask_similar limit 20')
+        rows = c.fetchall()
+        for row in rows:
+    
 
-
-        # f = open('sample.binaryfile','rb')
-        # com_img_dists = pickle.load(f)
-
-
-
-
-        comparing_files = img_dir_files
-        for comparing_file in comparing_files:
-            if comparing_file == '.DS_Store':
+            if not row[1].endswith(('.png', '.jpg', '.jpeg')):
                 continue
-            if not comparing_file.endswith(('.png', '.jpg', '.jpeg')):
-                continue
 
-            comparing_img_path = comparing_file
+            # comparing_img_path = imread_web(get_public_url(bucket,"actress/"+ row[1]))
             try:
-                comparing_img = cv2.imread(comparing_img_path, cv2.IMREAD_GRAYSCALE)
-                comparing_img = cv2.resize(comparing_img, img_size)
-                (_, comparing_des) = detector.detectAndCompute(comparing_img, None)
-                matches = bf.match(target_des, comparing_des)
-                # matches = bf.match(target_des, com_img_dists[comparing_file])
+                # comparing_des = 0
+                # comparing_img = comparing_img_path
+                # comparing_img = cv2.resize(comparing_img, img_size)
+                # (_, comparing_des) = detector.detectAndCompute(comparing_img, None)
+                # matches = bf.match(target_des, comparing_des)
+                numpy_img_data = np.array(row[2][row[1]]).astype(np.uint8)
+                matches = bf.match(target_des, numpy_img_data)
                 dist = [m.distance for m in matches]
                 score = sum(dist) / len(dist)
                 if score <= 100:
@@ -202,37 +224,24 @@ def upload():
                 score = 100.0 / score
             except cv2.error:
                 score = 100000
-            com_img_dists[comparing_file] = comparing_des
-            ret[comparing_file] = score
-        f = open('sample.binaryfile','wb')
-        pickle.dump(com_img_dists,f)
-        f.close
+            ret[row[1]] = score
+        # f = open('sample.binaryfile','wb')
+        # pickle.dump(com_img_dists,f)
+        # f.close
+        conn.close()
 
     ############################################################
     
     dic_sorted = sorted(ret.items(), reverse=True,key=lambda x:x[1])[:3]
-    estimated_d=[]
+    estimated_d =[]
+    exists_img =[]
     for file in dic_sorted:
-        img_path = cloudinary.CloudinaryImage('actress/' + file[0])
-        img = cv2.imread(img_path)
-        # cv2.imshow('image',img)
-                # 保存
-        dt_now = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_%f")
-        save_path = os.path.join(SAVE_DIR, dt_now + ".jpeg")
-        cv2.imwrite(save_path, img)
+        img_path = get_public_url(AWS_STORAGE_BUCKET_NAME,"actress/"+ file[0])
+        # obj = s3.Object(AWS_STORAGE_BUCKET_NAME, "actress/"+ file[0])
+        print(img_path)
         estimated_d.append(file[1])
-    f_imgs = os.listdir(SAVE_DIR)
-    if '.DS_Store' in f_imgs:
-        f_imgs.remove('.DS_Store')
-    exists_img = sorted(f_imgs)[-3:]
+        exists_img.append(img_path)
         
-        
-
-    
-
-    # ファイルの保存
-    # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # アップロード後のページに転送
     return render_template('index.html',img_url=img_url, data= zip(exists_img,estimated_d))
 
 
@@ -366,8 +375,10 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    # app.debug = True
+    # app.run()
+    import ssl
+    app.run(host='0.0.0.0', port=5955, ssl_context=('server.crt', 'server.key'), threaded=True, debug=True)
 
     
 
