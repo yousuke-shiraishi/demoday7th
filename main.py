f689f385053385383bbb276e49be4abf7340c628
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2019-09-01 10:57:12 +0900

    callbackの環境変数typo

--- main.py
+++ main.py
@@ -182,14 +182,14 @@ app = Flask(__name__)
 #     return redirect('/login')
 ###########################################################
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.environ('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.environ('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+channel_secret = os.environ['LINE_CHANNEL_SECRET']
+channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+# if channel_secret is None:
+#     print('Specify LINE_CHANNEL_SECRET as environment variable.')
+#     sys.exit(1)
+# if channel_access_token is None:
+#     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+#     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
