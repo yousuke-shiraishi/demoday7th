af8df234960a5e737b7e9d3793ad5691d0eed421
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2019-09-06 10:55:06 +0900

    最終確認V1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask import Flask
 # ファイル名をチェックする関数
 from werkzeug.utils import secure_filename
 # 画像のダウンロード
-# import json
+import json
 from flask import send_from_directory
 import numpy as np
 import cv2
@@ -17,10 +17,7 @@ import glob
 from PIL import Image
 from botocore.client import Config
 # from flask.logging import create_logger
-# from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-# from flask_wtf import Form
-# from wtforms import TextField, PasswordField, validators
-# import ldap
+from flask_httpauth import HTTPDigestAuth
 from io import BytesIO
 
 import os
@@ -39,168 +36,171 @@ from linebot.models import (
 )
 
 from os.path import join, dirname
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import psycopg2
 import boto3
 
 # load_dotenv(verbose=True)
-
+# POSTG_ID = os.environ['PG_ID']
+# POSTG_PW = os.environ['PG_PW']
+# POSTG_DB = os.environ['PG_DB']
 
 # dotenv_path = join(dirname(__file__), '.env')
 # load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 # LOG = create_logger(app)
-# login_manager = LoginManager()
-# login_manager.login_view =  "login"
-# login_manager.init_app(app)
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_REGION_NAME = os.environ['AWS_REGION_NAME']
+USER_ID = os.environ['USER_ID']
+USER_PD = os.environ['USER_PD']
+app.secret_key = os.environ['SECRET_KEY']
+DATABASE_URL = os.environ['DATABASE_URL']
+auth = HTTPDigestAuth()
 
-# app.config['SECRET_KEY'] = "secret"
-# app.config['LDAP_URL'] = 'ldap://localhost:389'
-# app.config['LDAP_DN_FORMAT'] = 'cn=%s,ou=Users,dc=example,dc=com'
+users = {
+    USER_ID: USER_PD
+}
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-# class User(object):
-#     def __init__(self, username, data=None):
-#         self.username = username
-#         self.data = data
-#     def is_authenticated(self):
-#         return True
-#     def is_active(self):
-#         return True
-#     def is_anonymous(self):
-#         return False
-#     def get_id(self):
-#         return self.username
-
-#     @classmethod
-#     def auth(cls, username, password):
-#         l = ldap.initialize(app.config['LDAP_URL'])
-#         dn = app.config['LDAP_DN_FORMAT'] % (username)
-#         try:
-#             l.simple_bind_s(dn, password)
-#         except:
-#             return None
-#         return User(username)
-
-# class LoginForm(Form):
-#     username = TextField('Username', validators=[validators.Required()])
-#     password = PasswordField('Password', validators=[validators.Required()])
-
-#     def __str__(self):
-#         return '''
-# <form action="/login" method="POST">
-# <p>%s: %s</p>
-# <p>%s: %s</p>
-# %s
-# <p><input type="submit" name="submit" /></p>
-# </form>
-# ''' % (self.username.label, self.username,
-#        self.password.label, self.password,
-#        self.csrf_token)
-
-# @login_manager.user_loader
-# def load_user(username):
-#     return User(username)
-
-# @app.route('/admin')
-# @login_required
-# def index_user():
-#     # カーソル作成
-#     conn = psycopg2.connect(
-#         host = "127.0.0.1",
-#         port = 5432,
-#         database="flask-similar",
-#         user="postgres",
-#         password="postgres")
-#     cur = conn.cursor()
-
-#     ins_img_path = request.files['insert_img']
-#     ins_img_url = ins_img_path[0]
-#     if not ins_img_path.endswith(('.png', '.jpg', '.jpeg')):
-#         print('Specify LINE_CHANNEL_SECRET as environment variable.')
-#         sys.exit(1)
-#     stream = ins_img_path.stream
-#     img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
-#     img = cv2.imdecode(img_array, 1)
-#     img_size = (200, 200)
-#     target_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-#     target_img = cv2.resize(target_img, img_size)
-
-#     bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-#     # detector = cv2.ORB_create()
-#     detector = cv2.AKAZE_create()
-#     (_, target_des) = detector.detectAndCompute(target_img, None)
-#     # SQLコマンド実行 (プレースホルダー使用。エスケープも勝手にされる)
-#     cur.execute("INSERT INTO flask_similar (image_name, image_json_feature) VALUES (%s, %s)", (ins_img_url, json.dumps({ins_img_url:target_des})))
-#     # SQL結果を受け取る
-#     # コミット
-#     conn.commit()
-#     # クローズ
-#     cur.close()
-#     conn.close()
-
-# def index_user():
-#     # カーソル作成
-#     conn = psycopg2.connect(
-#         host = "127.0.0.1",
-#         port = 5432,
-#         database="flask-similar",
-#         user="postgres",
-#         password="postgres")
-#     cur = conn.cursor()
-
-#     ins_img_path = request.files['insert_img']
-#     # SQLコマンド実行 (プレースホルダー使用。エスケープも勝手にされる)
-#     cur.execute("INSERT INTO flask_similar (image_name, image_json_feature) VALUES (%s, %s)", (ins_img_url, json.dumps({ins_img_url:target_des})))
-#     # SQL結果を受け取る
-#     # コミット
-#     conn.commit()
-#     # クローズ
-#     cur.close()
-#     conn.close()
+@app.route('/admin', methods=['GET','POST'])
+@auth.login_required
+def admin():
+    return render_template("admin.html")
 
 
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/admin_ins', methods=['GET','POST'])
+@auth.login_required
+def admin_ins():
+    # カーソル作成
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # conn = psycopg2.connect(
+    # host = "0.0.0.0",
+    # port = 5432,
+    # database=POSTG_DB,
+    # user=POSTG_ID,
+    # password=POSTG_PW)
+    
+    cur = conn.cursor()
+    ##############################################
+    
+    if 'insert_img1' not in request.files:
+        flash('ファイルがありません','failed')
+        return redirect(request.url)
+    fileToUpload = request.files['insert_img1']
+                # ファイルのチェック
+    if fileToUpload and allowed_file(fileToUpload.filename):
+        fileToUpload = secure_filename(fileToUpload.filename)
+
+    shutil.rmtree(SAVE_DIR)
+    os.mkdir(SAVE_DIR)
+    file_url = str(fileToUpload)
+    ins_str = file_url.split(" ")
+    ins_img_url = ins_str[1][1:-1]
+    i = Image.open(fileToUpload)
+    save_path = SAVE_DIR +"/" + ins_img_url
+    i.save(save_path)
+    filename = os.listdir(SAVE_DIR +"/")
+    img_size = (200, 200)
+    filename1 = SAVE_DIR +"/" + filename[0]
+    
+    target_img = cv2.imread(filename1, cv2.IMREAD_GRAYSCALE)
+    target_img = cv2.resize(target_img, img_size)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    # detector = cv2.ORB_create()
+    detector = cv2.AKAZE_create()
+    (_, target_des) = detector.detectAndCompute(target_img, None)
+    # SQLコマンド実行 (プレースホルダー使用。エスケープも勝手にされる)
+
+    cur.execute("INSERT INTO flask_similar (image_name, image_json_feature) VALUES (%s, %s)", (filename[0], json.dumps({filename[0]:target_des.tolist()})))
+    # SQL結果を受け取る
+    # コミット
+    # カーソル作成
+    conn.commit()
+    # クローズ
+    cur.close()
+    conn.close()
+    s3 = boto3.resource('s3') #S3オブジェクトを取得
+
+    bucket = s3.Bucket(AWS_STORAGE_BUCKET_NAME)
+    print("filename[0]",filename[0])
+    bucket.upload_file(filename1, 'actress/' + filename[0])
+###################################################
+
+    return render_template("admin.html")
+     
+
+@app.route('/admin_del', methods=['GET','POST'])
+@auth.login_required
+def admin_del():
+    if request.method == "GET":
+        return """
+            <h1>
+                ファイルを削除してデータベースを更新
+            </h1>
+            <form action="/admin_del" method = post>
+                <p><input type="text" name="delete_img1"></p>
+                <input type = submit value = delete_img>
+            </form>
+        """
+    else:
+        # カーソル作成
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        # conn = psycopg2.connect(
+        # host = "0.0.0.0",
+        # port = 5432,
+        # database=POSTG_DB,
+        # user=POSTG_ID,
+        # password=POSTG_PW)
+    
+        cur = conn.cursor()
+        del_img_url = request.form['delete_img1']
+        
+        # SQLコマンド実行 (プレースホルダー使用。エスケープも勝手にされる)
+        s3_client = boto3.client('s3')
+        s3_client.delete_object(Bucket=AWS_STORAGE_BUCKET_NAME, Key='actress/' + del_img_url)
+        
+        sql = 'DELETE FROM flask_similar WHERE image_name = %s'
+        cur.execute(sql, (del_img_url,))
+        
+        # SQL結果を受け取る
+        # コミット
+        conn.commit()
+        # クローズ
+        cur.close()
+        conn.close()
+        return """
+            <h1>
+                ファイルを削除してデータベースを更新
+            </h1>
+            <form action="/admin_del" method = post>
+                <p><input type="text" name="delete_img1"></p>
+                <input type = submit value = delete_img>
+            </form>
+        """
 
 
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm(request.form)
-#     if form.validate_on_submit():
-#         user = User.auth(form.username.data, form.password.data)
-#         if user:
-#             login_user(user)
-#             print('Login successfully.')
-#             return redirect(request.args.get('next', '/'))
-#         else:
-#             print('Login failed.')
-#     return str(form)
-
-# @app.route("/logout")
-# def logout():
-#     logout_user()
-#     return redirect('/login')
 ###########################################################
-# get channel_secret and channel_access_token from your environment variable
 channel_secret = os.environ['LINE_CHANNEL_SECRET']
 channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-# if channel_secret is None:
-#     print('Specify LINE_CHANNEL_SECRET as environment variable.')
-#     sys.exit(1)
-# if channel_access_token is None:
-#     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-#     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-
-
 
 SUB_DIR = 'actress/'
-app.secret_key = os.environ['SECRET_KEY']
+
 SAVE_DIR = "./images"
 if not os.path.isdir(SAVE_DIR):
     os.mkdir(SAVE_DIR)
@@ -211,30 +211,19 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app.config['UPLOAD_FOLDER'] = SAVE_DIR
 
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_REGION_NAME = os.environ['AWS_REGION_NAME']
-
-
-# POSTG_ID = os.environ['PG_ID']
-# POSTG_PW = os.environ['PG_PW']
-# POSTG_DB = os.environ['PG_DB']
 
 estimated_d =[]
 img1 =[]
 exists_img=[]
 img_url=""
 
-# s3 = boto3.client('s3')
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 
 @app.route('/images/<path:path>')
 def send_js(path):
     return send_from_directory(SAVE_DIR, path)
+
 
 # ファイルを受け取る方法の指定
 @app.route('/', methods=['GET','POST'])
@@ -246,25 +235,28 @@ def index():
     else:
         return render_template("index.html",img_url=img_url, data= zip(exists_img,estimated_d))
 
-
+###########
 
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # conn = psycopg2.connect(
+    # host = "0.0.0.0",
+    # port = 5432,
+    # database=POSTG_DB,
+    # user=POSTG_ID,
+    # password=POSTG_PW)
     shutil.rmtree(SAVE_DIR)
     os.mkdir(SAVE_DIR)
     s3 = boto3.client('s3', region_name='ap-northeast-1',config=Config(signature_version='s3v4'))
     # # ファイルがなかった場合の処理
-    # if 'file' not in request.files:
-    #     flash('ファイルがありません','failed')
-    #     return redirect(request.url)
-    # file = request.files['image']
-    #             # ファイルのチェック
-    # if file and allowed_file(file.filename):
-        # 危険な文字を削除（サニタイズ処理）
-        # filename = secure_filename(file.filename)
-    # 画像として読み込み
+    if 'image' not in request.files:
+        flash('ファイルがありません','failed')
+        return redirect(request.url)
     img1 = request.files['image']
+                # ファイルのチェック
+    if img1 and allowed_file(img1.filename):
+        img1 = secure_filename(img1.filename)
     stream = img1.stream
     img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
     img = cv2.imdecode(img_array, 1)
@@ -328,7 +320,12 @@ def upload():
         ExpiresIn = 60,
         HttpMethod = 'GET')
         
-        estimated_d.append(file[1])
+        if file[1] >= 0.85:
+            estimated_d.append("類似度 高")
+        elif file[1] >= 0.8:
+            estimated_d.append("類似度 中")
+        else:
+            estimated_d.append("類似度 低")
         exists_img.append(img_path)
         
     return render_template('index.html',img_url=img_url, data= zip(exists_img,estimated_d))
@@ -359,19 +356,16 @@ def handle_image_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
     shutil.rmtree(SAVE_DIR)
     os.mkdir(SAVE_DIR)
-    print("コンテンツ",message_content)
     i = Image.open(BytesIO(message_content.content))
     save_path = SAVE_DIR +"/" + event.message.id + '.jpg'
     i.save(save_path)
     filename = os.listdir(SAVE_DIR +"/")
-    # img_array = np.asarray(bytes(str(filename),'utf8'), dtype=np.uint8)
-    # img = cv2.imdecode(img_array, 1)
     img_size = (200, 200)
     ret = {}
 
 #     #####################################
     if SUB_DIR == 'actress/':
-        print(filename)
+
         filename = SAVE_DIR +"/" + filename[0]
         target_img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
         target_img = cv2.resize(target_img, img_size)
